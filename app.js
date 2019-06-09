c64f59b73be1fc74459886b8932731805ae3442e
Jose Fernandez <jos.frndz@gmail.com>
2019-06-09 02:35:24 -0400

    Add express, ejs, nodemon modules, root path.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to YelpCamp!');
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('Server has started!');
+});
