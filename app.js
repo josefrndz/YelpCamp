d3dee2ecf1185f2eff65ff1e7aeb0d0415867ce1
Jose Fernandez <jos.frndz@gmail.com>
2019-06-09 03:17:43 -0400

    Fix nodemon via scripts. Set up Landing Page & CGs

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Welcome to YelpCamp!');
+  res.render('landing');
 });
 
-app.listen(process.env.PORT, process.env.IP, () => {
-  console.log('Server has started!');
+app.get('/campgrounds', (req, res) => {
+  var campgrounds = [
+    {
+      name: 'Inwood Creek',
+      image: 'https://www.nycgovparks.org/photo_gallery/full_size/9730.jpg'
+    },
+    {
+      name: 'Washington Top',
+      image:
+        'https://www.nycgovparks.org/pagefiles/107/inwood-hill-park-map-rock-formations__57d2ba470ac0e.jpg'
+    },
+    {
+      name: 'Payson Hill',
+      image: 'https://www.nycgovparks.org/photo_gallery/full_size/9711.jpg'
+    }
+  ];
+  res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, process.env.IP, () => {
+  console.log(`Server has started on PORT ${PORT}`);
 });
